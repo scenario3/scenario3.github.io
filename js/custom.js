6711fa7c706555c50ef4f4aab964fb377e22e431
Matt Policane <mattpolicane@gmail.com>
2016-11-02 20:20:06 +0000

    All input can be used:

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,11 @@ function getRefDetails() {
   var refLink = document.getElementById('link').value;
   var refComment = document.getElementById('comment').value;
 }
+
+function getNewDetails() {
+  var firstName = document.getElementById('first_name').value;
+  var lastName = document.getElementById('last_name').value;
+  var emailAddress = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var passwordConf = document.getElementById('password_confirmation').value;
+}
