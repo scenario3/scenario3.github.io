a68062b8350b3d923518f6392897c5cdd139f26b
Matt Policane <mattpolicane@gmail.com>
2016-11-02 20:08:28 +0000

    Second commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+function getAccountDetails() {
+  var emailAddress = document.getElementById('inputEmail').value;
+  var emailAddress = document.getElementById('inputEmail').value;
+}
+
+function getRefDetails() {
+  var refTitle = document.getElementById('title').value;
+  var refLink = document.getElementById('link').value;
+  var refComment = document.getElementById('comment').value;
+}
